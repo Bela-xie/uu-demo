477c588fb73f9cac38db2b49da33edfb1981398e
bela <1981253197@qq.com>
2020-09-20 22:46:00 +0800

    发布到了npm上

---
+++ index.js
@@ -0,0 +1,9 @@
+import Button from './src/components/Button.vue'
+import ButtonGroup from './src/components/ButtonGroup.vue'
+import Icon from './src/components/Icon.vue'
+
+export default {
+    Button,
+    ButtonGroup,
+    Icon
+}
