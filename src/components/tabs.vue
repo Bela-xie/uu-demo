85a87945d4473d63312d37385a76093aac918403
bela <1981253197@qq.com>
2020-09-26 10:04:25 +0800

    重构tabs组件

--- src/components/tabs.vue
+++ src/components/tabs.vue
@@ -10,48 +10,49 @@ export default {
   name: "UUTabs",
   data() {
     return {
-      eventBus: new Vue()
+      eventBus: new Vue(),
     };
   },
   provide() {
     return {
-      eventBus: this.eventBus
+      eventBus: this.eventBus,
     };
   },
-  mounted() {
-    if (this.$children.length === 0) {
-      console &&
-        console.warn &&
-        console.warn(
-          "u-tabs下应该有子组件u-head和u-body，但是你并没有写子组件"
-        );
-    }
-    this.$children.forEach(vm => {
-      if (vm.$options.name === "UUTabsHead") {
-        vm.$children.forEach(childVm => {
-          if (
-            childVm.name === this.selected &&
-            childVm.$options.name === "UUTabsItem"
-          ) {
-            this.eventBus.$emit("update:selected", this.selected, childVm);
-          }
-        });
+  methods: {
+    checkChildren() {
+      if (this.$children.length === 0) {
+        console && console.warn && console.warn("u-tabs下应该有子组件u-head和u-body，但是你并没有写子组件");
       }
-    });
+    },
+    selectTab() {
+      this.$children.forEach((vm) => {
+        if (vm.$options.name === "UUTabsHead") {
+          vm.$children.forEach((childVm) => {
+            if (childVm.name === this.selected && childVm.$options.name === "UUTabsItem") {
+              this.eventBus.$emit("update:selected", this.selected, childVm);
+            }
+          });
+        }
+      });
+    },
+  },
+  mounted() {
+    this.checkChildren();
+    this.selectTab();
   },
   props: {
     selected: {
       type: String,
-      required: true
+      required: true,
     },
     direction: {
       type: String,
       default: "horizontal",
       validater(value) {
         return ["horizontal", "vertical"].indexOf(value) >= 0;
-      }
-    }
-  }
+      },
+    },
+  },
 };
 </script>
 
